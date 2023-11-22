576c82865a60f2091e2106ec3c4dc7ac050d95ea
JyrwaJR <jyrwaboys@gmail.com>
2023-11-22 23:49:29 +0530

    create register route

--- src/app/config/firebase.ts
+++ src/app/config/firebase.ts
@@ -39,7 +39,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const imgDB = getStorage(app);
-const db = getFirestore(app);
 
-export { db, imgDB };
+export { imgDB };
+export const db = getFirestore(app);
 export const auth = getAuth(app);
